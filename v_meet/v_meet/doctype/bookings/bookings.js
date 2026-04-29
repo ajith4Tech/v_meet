67ea4a08ca2f0bc8a0ad302f04f72a541224d12d
ajith4Tech <ajithbm01@gmail.com>
2026-04-29 19:39:26 +0000

    Feat: Added Bookings, Rooms doctype

---
+++ v_meet/v_meet/doctype/bookings/bookings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, ajithbm01@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Bookings", {
+// 	refresh(frm) {
+
+// 	},
+// });
